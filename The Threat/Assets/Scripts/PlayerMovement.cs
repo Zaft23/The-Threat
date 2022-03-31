b75790a97006857df8350ed027d7bdfacd2646c2
Zaft23 <nadhil_ammar@hotmail.com>
2022-03-31 15:56:48 +0700

    Push: - Basic left and right movement added - Basic Jump control and ground check added

--- The Threat/Assets/Scripts/PlayerMovement.cs
+++ The Threat/Assets/Scripts/PlayerMovement.cs
@@ -2,20 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerMovement : MonoBehaviour
 {
 
-    public CharacterController controller;
+    private Rigidbody2D rb2D;
+    //private Animator myAnimator;
+    //will need animator when we have proper assets
 
-    // Start is called before the first frame update
+    [Header("Public Vars")]
+    public float Speed;
+    public float HorizontalMovement;
+    public float JumpForce;
+    public bool IsGrounded;
+
+    [Header("Private Vars")]
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private float _radOCircle;
+    [SerializeField] private LayerMask _whatIsGround;
+
     void Start()
     {
-        
+        //
+        rb2D = GetComponent<Rigidbody2D>();
+        //myAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        HorizontalMovement = Input.GetAxisRaw("Horizontal");
+        //HorizontalMovement = Input.GetAxis("Horizontal");
+        //might need to check the difrrent feels of axis and axisraw
+
+
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(_groundCheck.position, _radOCircle);
+    }
+
+    void FixedUpdate()
+    {
+        rb2D.velocity = new Vector2(HorizontalMovement * Speed, rb2D.velocity.y);
+
+
+        IsGrounded = Physics2D.OverlapCircle(_groundCheck.position, _radOCircle, _whatIsGround);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded)
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, JumpForce);
+        }
+
+    }
+
 }
