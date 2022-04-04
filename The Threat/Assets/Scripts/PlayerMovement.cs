6f8b72f3de064c22f827a685140538ea0ad4eb8b
Zaft23 <nadhil_ammar@hotmail.com>
2022-04-04 16:20:47 +0700

    Push: - Player can now Dash - Player has buffer jump, player can start jump again a few cm before they hit ground - Payer has Coyote Jump, player can start jump a few cm off platform - player has corner check, if a little part of player's body hit a platform -> player's object will nudge player off the platform above them thus still allowing player jump even though their little part of the body hit a platform - better gravity control for player, thus making their jump less floaty

--- The Threat/Assets/Scripts/PlayerMovement.cs
+++ The Threat/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-   
+
     [Header("Components")]
     private Rigidbody2D rb2D;
     //private Animator myAnimator;
@@ -22,29 +22,55 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask _whatIsGround;
     public bool IsGrounded;
 
-   // [Header("Gravity")]
+    
+    [Header("Dashing")]
+    [SerializeField] private float _dashingVelocity = 14f;
+    [SerializeField] private float _dashingTime = 0.5f;
+    private Vector2 _dashingDir;
+    private bool _isDashing;
+    private bool _canDash = true;
+    //private TrailRenderer _trailRenderer;
+    //TEST
+
+
+    [Header("Jump Gravity")]
+    public float FallMupltiplier = 2.5f;
+    public float LowJumpMultiplier = 2f;
+
 
     [Header("Jump details")]
-    //jump height
     public float JumpForce;
     [SerializeField] private float _yVelJumpReleasedMod = 2F;
+    [SerializeField] private float _fJumpPressedRememberTime = 0.2f;
+    [SerializeField] private float _fJumpPressedRemember = 0;
+    [SerializeField] float _coyoteRemember = 0;
+    [SerializeField] float _coyoteRememberTime = 0.3f;
+
+    [Header("Corner Correction Variables")]
+    [SerializeField] private float _topRaycastLength;
+    [SerializeField] private Vector3 _edgeRaycastOffset;
+    [SerializeField] private Vector3 _innerRaycastOffset;
+    [SerializeField] private LayerMask _cornerCorrectLayer;
+    private bool _canCornerCorrect;
 
 
-    void Start()
+
+
+
+    void Awake()
     {
         //
         rb2D = GetComponent<Rigidbody2D>();
         //myAnimator = GetComponent<Animator>();
+        //_trailRenderer = GetComponent<TrailRenderer>();
 
-       
 
     }
-    
+
     void Update()
     {
         horizontalMovement = Input.GetAxisRaw("Horizontal");
-        //HorizontalMovement = Input.GetAxis("Horizontal");
-        //might need to check the difrrent feels of axis and axisraw
+
 
 
     }
@@ -52,22 +78,96 @@ public class PlayerMovement : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.DrawSphere(_groundCheck.position, _radOCircle);
+        //Corner Check
+        Gizmos.DrawLine(transform.position + _edgeRaycastOffset, transform.position + _edgeRaycastOffset + Vector3.up * _topRaycastLength);
+        Gizmos.DrawLine(transform.position - _edgeRaycastOffset, transform.position - _edgeRaycastOffset + Vector3.up * _topRaycastLength);
+        Gizmos.DrawLine(transform.position + _innerRaycastOffset, transform.position + _innerRaycastOffset + Vector3.up * _topRaycastLength);
+        Gizmos.DrawLine(transform.position - _innerRaycastOffset, transform.position - _innerRaycastOffset + Vector3.up * _topRaycastLength);
+
+        //Corner Distance Check
+        Gizmos.DrawLine(transform.position - _innerRaycastOffset + Vector3.up * _topRaycastLength,
+                        transform.position - _innerRaycastOffset + Vector3.up * _topRaycastLength + Vector3.left * _topRaycastLength);
+        Gizmos.DrawLine(transform.position + _innerRaycastOffset + Vector3.up * _topRaycastLength,
+                        transform.position + _innerRaycastOffset + Vector3.up * _topRaycastLength + Vector3.right * _topRaycastLength);
     }
    
     void FixedUpdate()
     {
         
+        var dashInput = Input.GetButtonDown("Dash");
+
+        if(dashInput && _canDash)
+        {
+            _isDashing = true;
+            _canDash = false;
+            _dashingDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            //_trailRenderer.emitting = true;
+            if (_dashingDir == Vector2.zero)
+            {
+                _dashingDir = new Vector2(transform.localScale.x, 0);
+            }
+
+            StartCoroutine(_StopDashing());
+
+        }
+
+        //animator.SetBool("IsDashing", _isDashing);
+
+        if (_isDashing)
+        {
+            rb2D.velocity = _dashingDir.normalized * _dashingVelocity;
+            return;
+        }
+
+        if (IsGrounded)
+        {
+            _canDash = true;
+        }
+
+
+       
+
+        CheckCollisions();
+
         //Handles basic X axis Movement
         rb2D.velocity = new Vector2(horizontalMovement * Speed, rb2D.velocity.y);
 
-        //Handles ground check and Y Axis (Jump) Movement
+        // Jump Physics
         IsGrounded = Physics2D.OverlapCircle(_groundCheck.position, _radOCircle, _whatIsGround);
 
-        // Jump Control using Variable Height Jump
+        _coyoteRemember -= Time.deltaTime;
+        if (IsGrounded)
+        {
+            _coyoteRemember = _coyoteRememberTime;
+        }
+
+        _fJumpPressedRemember -= Time.deltaTime;
+        if (Input.GetButtonDown("Jump"))
+        {
+            _fJumpPressedRemember = _fJumpPressedRememberTime;
+        }
+
         if (Input.GetButtonDown("Jump") && IsGrounded)
         {
             rb2D.velocity = new Vector2(rb2D.velocity.x, y: JumpForce);
+        }
 
+        //Allow player to jump a few cm off from ground after jump
+        if ((_fJumpPressedRemember > 0) && (_coyoteRemember > 0))
+        {
+            _fJumpPressedRemember = 0;
+            _coyoteRemember = 0;
+            rb2D.velocity = new Vector2(rb2D.velocity.x, y: JumpForce);
+
+        }
+
+        if (rb2D.velocity.y < 0)
+        {
+            rb2D.velocity += Vector2.up * Physics2D.gravity.y * (FallMupltiplier -1) * Time.deltaTime;
+        }
+        else if(rb2D.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb2D.velocity += Vector2.up * Physics2D.gravity.y * (LowJumpMultiplier - 1) * Time.deltaTime;
         }
 
         if (Input.GetButtonUp("Jump") && rb2D.velocity.y > 0)
@@ -75,15 +175,72 @@ public class PlayerMovement : MonoBehaviour
             rb2D.velocity = new Vector2(rb2D.velocity.x, y:rb2D.velocity.y / _yVelJumpReleasedMod);
         }
 
+        //Corner Correction
+        if (_canCornerCorrect) CornerCorrect(rb2D.velocity.y);
+
+      
+
+
+
         //found from another tutorial https://www.youtube.com/watch?v=Mo1-sKYbks0&ab_channel=PitiIT i assume it's to control the basic X axis movement for now just ignore
         //if (horizontalMovement != 0)
         //{
         //    transform.localScale = new Vector3(x: Mathf.Sign(horizontalMovement), y: 1, z: 1);
         //}
 
+    }
+
+    //Corner Correction
+    void CornerCorrect(float yVelocity)
+    {
+        //Push player to the right
+        RaycastHit2D _hit = Physics2D.Raycast(transform.position - _innerRaycastOffset + Vector3.up * _topRaycastLength, Vector3.left, _topRaycastLength, _cornerCorrectLayer);
+        if (_hit.collider != null)
+        {
+            float _newPos = Vector3.Distance(new Vector3(_hit.point.x, transform.position.y, 0f) + Vector3.up * _topRaycastLength,
+                transform.position - _edgeRaycastOffset + Vector3.up * _topRaycastLength);
+            transform.position = new Vector3(transform.position.x + _newPos, transform.position.y, transform.position.z);
+            rb2D.velocity = new Vector2(rb2D.velocity.x, yVelocity);
+            return;
+        }
+
+        //Push player to the left
+        _hit = Physics2D.Raycast(transform.position + _innerRaycastOffset + Vector3.up * _topRaycastLength, Vector3.right, _topRaycastLength, _cornerCorrectLayer);
+        if (_hit.collider != null)
+        {
+            float _newPos = Vector3.Distance(new Vector3(_hit.point.x, transform.position.y, 0f) + Vector3.up * _topRaycastLength,
+                transform.position + _edgeRaycastOffset + Vector3.up * _topRaycastLength);
+            transform.position = new Vector3(transform.position.x - _newPos, transform.position.y, transform.position.z);
+            rb2D.velocity = new Vector2(rb2D.velocity.x, yVelocity);
+        }
+    }
+
+    void CheckCollisions()
+    {
+
+        //Corner Collisions
+        _canCornerCorrect = Physics2D.Raycast(transform.position + _edgeRaycastOffset, Vector2.up, _topRaycastLength, _cornerCorrectLayer) &&
+        !Physics2D.Raycast(transform.position + _innerRaycastOffset, Vector2.up, _topRaycastLength, _cornerCorrectLayer) ||
+        Physics2D.Raycast(transform.position - _edgeRaycastOffset, Vector2.up, _topRaycastLength, _cornerCorrectLayer) &&
+        !Physics2D.Raycast(transform.position - _innerRaycastOffset, Vector2.up, _topRaycastLength, _cornerCorrectLayer);
 
 
 
     }
+
+    private IEnumerator _StopDashing()
+    {
+        yield return new WaitForSeconds(_dashingTime);
+        //_trailRenderer.emitting = false;
+        _isDashing = false;
+
+
+    }
+
+
+  
+
+
+
 
 }
